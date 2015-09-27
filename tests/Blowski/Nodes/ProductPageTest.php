8178b76d943691b1b68aef04a6ac1a18044266cf
Dan Blows <d@nblo.ws>
2015-09-27 23:24:34 +0100

    Product page test added

--- tests/Blowski/Nodes/ProductPageTest.php
+++ tests/Blowski/Nodes/ProductPageTest.php
@@ -3,14 +3,18 @@
 namespace Blowski\Tests\Nodes;
 
 use Blowski\Nodes\ProductPage as SUT;
+use Symfony\Component\DomCrawler\Crawler;
 
 class ProductPageTest extends \PHPUnit_Framework_TestCase
 {
 
-    /** @todo */
-    public function test_it_passes()
+    public function test_it_extracts_description()
     {
-
+        $html = file_get_contents(__DIR__.'/../Fixtures/product-page.html');
+        $crawler = new Crawler();
+        $crawler->addHtmlContent($html, 'ISO-8859-1');
+        $SUT = new SUT($crawler);
+        $this->assertEquals("Apricots", $SUT->getDescription());
     }
 
 }
